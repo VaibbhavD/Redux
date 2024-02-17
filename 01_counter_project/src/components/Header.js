e4a511e9c928587a78e28e85da36db299811ea23
VaibhavDhamanage <vaibhavdhamanage>
2024-02-17 10:00:36 +0530

    add conditional showing header menu

--- 01_counter_project/src/components/Header.js
+++ 01_counter_project/src/components/Header.js
@@ -1,21 +1,32 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { AuthActions } from "../store/store";
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
+  const isLoggedin = useSelector((state) => state.Auth.IsAuthenticated);
+  const dispatch = useDispatch();
+
+  const LogoutHandler = () => {
+    dispatch(AuthActions.Logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {isLoggedin && (
+          <ul>
+            <li>
+              <a>My Products</a>
+            </li>
+            <li>
+              <a>My Sales</a>
+            </li>
+            <li>
+              <button onClick={LogoutHandler}>Logout</button>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
