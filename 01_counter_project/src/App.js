e92989b378e10780da702e84b30353e472cb4b97
VaibhavDhamanage <vaibhavdhamanage>
2024-02-17 09:59:37 +0530

    Add Auth component

--- 01_counter_project/src/App.js
+++ 01_counter_project/src/App.js
@@ -1,7 +1,22 @@
 import Counter from "./components/Counter";
+import Auth from "./components/Auth";
+import { Fragment } from "react";
+import Header from "./components/Header";
+import { AuthActions } from "./store/store";
+import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  return <Counter />;
+  const isLoggedin = useSelector((state) => state.Auth.IsAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+      {!isLoggedin && <Auth />}
+      {isLoggedin && <UserProfile />}
+      <Counter />
+    </Fragment>
+  );
 }
 
 export default App;
