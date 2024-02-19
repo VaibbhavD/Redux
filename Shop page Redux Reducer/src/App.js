9a86463dfd706e80b71cd8d3996b642202e4c508
VaibhavDhamanage <vaibhavdhamanage>
2024-02-19 18:11:47 +0530

    Debug And Clean

--- Shop page Redux Reducer/src/App.js
+++ Shop page Redux Reducer/src/App.js
@@ -1,11 +1,10 @@
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
-import CartItem from "./components/Cart/CartItem";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
-  const showcart = useSelector((state) => state.ShowCart);
+  const showcart = useSelector((state) => state.UI.ShowCart);
 
   return (
     <Layout>
