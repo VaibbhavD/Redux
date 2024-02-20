660702946d01a98c5e9e922165c17cfc9b6eaa11
VaibhavDhamanage <vaibhavdhamanage>
2024-02-20 20:24:08 +0530

    Add fetch req for store CartItems

--- Shop page Redux Reducer/src/App.js
+++ Shop page Redux Reducer/src/App.js
@@ -1,13 +1,30 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import Notification from "./components/UI/Notification";
+import { SliceAction } from "./store Redux/Slice";
 
 function App() {
   const showcart = useSelector((state) => state.UI.ShowCart);
+  const Items = useSelector((state) => state.CartItem.items);
+  const ShoeNotification = useSelector((state) => state.UI.ShoeNotification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch(
+      "https://expense-tracker-7b7e1-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(Items),
+      }
+    );
+  }, [Items]);
 
   return (
     <Layout>
+      {ShoeNotification && <Notification />}
       {showcart && <Cart />}
       <Products />
     </Layout>
