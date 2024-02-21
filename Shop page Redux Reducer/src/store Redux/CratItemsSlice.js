c0af257fd69cabd0d2f58cf78e246c39b1430201
VaibhavDhamanage <vaibhavdhamanage>
2024-02-21 11:27:25 +0530

    Add new reducer repace item s form fetch items data

--- Shop page Redux Reducer/src/store Redux/CratItemsSlice.js
+++ Shop page Redux Reducer/src/store Redux/CratItemsSlice.js
@@ -9,6 +9,10 @@ const CartSlice = createSlice({
   name: "CartItems",
   initialState: CartItemsState,
   reducers: {
+    Replace: (state, action) => {
+      state.totalQty = action.payload.totalQty;
+      state.items = action.payload.items;
+    },
     AddtoItem: (state, action) => {
       const newitem = action.payload;
       const Expitem = state.items.find((Item) => newitem.id === Item.id);
@@ -28,7 +32,7 @@ const CartSlice = createSlice({
       }
     },
     RemoveItem: (state, action) => {
-      const Item = state.items.find((item) => item !== action.payload);
+      const Item = state.items.find((item) => item.id === action.payload.id);
       state.totalQty--;
       if (Item.Qty === 1) {
         state.items = state.items.filter((item) => item.id !== Item.id);
@@ -39,6 +43,7 @@ const CartSlice = createSlice({
     },
   },
 });
+
 export const CartAction = CartSlice.actions;
 
 const CartReducer = CartSlice.reducer;
